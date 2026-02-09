8f72efddbbe9d2c7645c43a5d6ee0c959af7aca0
Timotej Adamec <timotej.adamec@gendigital.com>
2026-02-09 15:27:41 +0100

    Fix missing imports in FindingDetailContent

--- feat/findings/fe/driving/impl/src/commonMain/kotlin/cz/adamec/timotej/snag/findings/fe/driving/impl/internal/findingDetail/ui/FindingDetailContent.kt
+++ feat/findings/fe/driving/impl/src/commonMain/kotlin/cz/adamec/timotej/snag/findings/fe/driving/impl/internal/findingDetail/ui/FindingDetailContent.kt
@@ -12,8 +12,10 @@
 
 package cz.adamec.timotej.snag.findings.fe.driving.impl.internal.findingDetail.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.calculateEndPadding
