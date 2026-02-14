a40bbe65a9f566ebcac8d458095215ffa818ae4a
Timotej Adamec <timotej.adamec@gendigital.com>
2026-02-14 02:12:53 +0100

    UI adjustments

--- feat/projects/fe/driving/impl/src/commonMain/kotlin/cz/adamec/timotej/snag/projects/fe/driving/impl/internal/projects/ui/components/ProjectListItem.kt
+++ feat/projects/fe/driving/impl/src/commonMain/kotlin/cz/adamec/timotej/snag/projects/fe/driving/impl/internal/projects/ui/components/ProjectListItem.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import cz.adamec.timotej.snag.lib.core.common.Timestamp
@@ -40,6 +41,7 @@ internal fun ProjectListItem(
     ListItem(
         modifier =
             modifier
+                .clip(shape = MaterialTheme.shapes.large)
                 .clickable(onClick = onClick),
         headlineContent = {
             Text(
