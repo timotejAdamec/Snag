931aaab50ab59a373285926816ca945349a32e1e
Timotej Adamec <timotej.adamec@gendigital.com>
2026-02-17 16:36:34 +0100

    Adjust finding type picker dialog

--- feat/findings/fe/driving/api/src/commonMain/kotlin/cz/adamec/timotej/snag/feat/findings/fe/driving/api/FindingTypePickerDialog.kt
+++ feat/findings/fe/driving/api/src/commonMain/kotlin/cz/adamec/timotej/snag/feat/findings/fe/driving/api/FindingTypePickerDialog.kt
@@ -13,11 +13,11 @@
 package cz.adamec.timotej.snag.feat.findings.fe.driving.api
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.AlertDialog
@@ -27,8 +27,11 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import cz.adamec.timotej.snag.feat.findings.business.FindingType
+import cz.adamec.timotej.snag.lib.design.fe.theme.SnagPreview
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import snag.feat.findings.fe.driving.api.generated.resources.Res
@@ -51,15 +54,16 @@ fun FindingTypePickerDialog(
             Text(text = stringResource(Res.string.select_finding_type))
         },
         text = {
-            Column {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
                 types.forEach { (type, key) ->
                     val visuals = type.visuals()
                     Row(
                         modifier =
                             Modifier
                                 .fillMaxWidth()
-                                .clickable { onTypeSelected(key) }
-                                .padding(vertical = 12.dp),
+                                .clickable { onTypeSelected(key) },
                         verticalAlignment = Alignment.CenterVertically,
                     ) {
                         Icon(
@@ -81,4 +85,15 @@ fun FindingTypePickerDialog(
             }
         },
     )
+}
+
+@Preview(device = Devices.PHONE)
+@Composable
+private fun FindingTypePickerDialogPreview() {
+    SnagPreview {
+        FindingTypePickerDialog(
+            onTypeSelected = {},
+            onDismiss = {},
+        )
+    }
 }
