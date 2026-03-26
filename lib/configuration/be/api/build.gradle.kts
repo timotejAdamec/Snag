db96dc210b3503c9bf0781e6e9ad653e4c968191
Timotej Adamec <46161280+timotejAdamec@users.noreply.github.com>
2026-03-26 19:13:47 +0100

    Apply suggestions from code review
    Co-authored-by: Timotej Adamec <46161280+timotejAdamec@users.noreply.github.com>

--- lib/configuration/be/api/build.gradle.kts
+++ lib/configuration/be/api/build.gradle.kts
@@ -5,5 +5,5 @@ plugins {
 dependencies {
     api(libs.ktor.server.core)
     api(libs.ktor.server.status.pages)
-    implementation(projects.lib.configuration.common.api)
+    api(projects.lib.configuration.common.api)
 }
