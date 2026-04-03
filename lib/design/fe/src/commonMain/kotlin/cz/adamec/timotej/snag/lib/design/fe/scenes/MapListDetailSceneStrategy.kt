e04246f51df6156c8c0bea1062e35f60e5f3e829
Timotej Adamec <timotej.adamec@gendigital.com>
2026-04-04 00:21:50 +0200

    Remove cached entry

--- lib/design/fe/src/commonMain/kotlin/cz/adamec/timotej/snag/lib/design/fe/scenes/MapListDetailSceneStrategy.kt
+++ lib/design/fe/src/commonMain/kotlin/cz/adamec/timotej/snag/lib/design/fe/scenes/MapListDetailSceneStrategy.kt
@@ -16,10 +16,6 @@ package cz.adamec.timotej.snag.lib.design.fe.scenes
 
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -32,9 +28,7 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.movableContentOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation3.runtime.NavEntry
@@ -161,11 +155,6 @@ private class AdaptiveMapListDetailScene<T : Any>(
                     label = "sheetPeekHeight",
                 )
 
-                var cachedDetailEntry by remember {
-                    mutableStateOf(detailEntry)
-                }
-                if (detailEntry != null) cachedDetailEntry = detailEntry
-
                 val scaffoldState =
                     rememberBottomSheetScaffoldState(
                         bottomSheetState =
@@ -196,7 +185,7 @@ private class AdaptiveMapListDetailScene<T : Any>(
                                 label = "sheetContent",
                             ) { showDetail ->
                                 if (showDetail) {
-                                    cachedDetailEntry?.Content()
+                                    detailEntry?.Content()
                                 } else {
                                     listEntry?.Content()
                                 }
